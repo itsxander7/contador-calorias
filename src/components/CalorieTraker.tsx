30089a96e1f8664aa88215dc52aa13254adfcfcc
Alexander Alvarez <125148489+itsxander7@users.noreply.github.com>
2025-01-28 09:44:10 -0600

    On branch master  Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/components/CalorieTraker.tsx

--- src/components/CalorieTraker.tsx
+++ src/components/CalorieTraker.tsx
@@ -18,7 +18,7 @@ export default function CalorieTraker({activities} : CalorieTrakerPops) {
   return (
     <>
       <h2 className="text-4xl font-black text-white text-center ">Resumen de Calorias</h2>
-      <div className="flex flex-col items-center  md:flex-row md:justify-between gap-5 mt-10">
+      <div className="flex flex-col items-center  md:flex-row md:justify-between gap-4 mt-10">
       < CaloriesDisplay 
       calories = {caloriesConsumed}
       text = "Consumidas"
